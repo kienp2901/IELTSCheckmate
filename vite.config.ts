344b82b5e71d9bb93cfb753196daaafd32f1633c
Phạm Cương <vkpan297@gmail.com>
2025-10-26 16:27:44 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wordpress/wp-content/plugins/ielts_checkmate_dashboard/dist/',
+  base: '/wp-content/plugins/ielts_checkmate_dashboard/dist/',
   build: {
     outDir: 'dist',
     // assetsDir: 'student-workspace-v3/assets',
